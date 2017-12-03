6532167aac899e008c3af1d2670902f140e519ef
KamonohashiPerry <economics.teru@gmail.com>
2017-12-03 16:32:59 +0900

    With Setting Names

--- r_advent_calendar_2017_pokemon.R
+++ r_advent_calendar_2017_pokemon.R
@@ -1,21 +1,24 @@
 library(rvest)
+library(tidyverse)
+library(magrittr)
 
+# htmlソースコードを読み込む
 pokemon_ranking <- read_html("https://yakkun.com/sm/status_list.htm")
 
-html_structure(pokemon_ranking)
-as_list(pokemon_ranking)
+# class属性がtdタグのノードを抽出
+node_extracted <- html_nodes(pokemon_ranking, "td")
+
+# ノードからテキストを抽出して行列にする
+pokemon_data <- data.frame(matrix(html_text(node_extracted),
+                       ncol = 9,byrow = TRUE))
+
+# 列名をつける
+pokemon_data <- pokemon_data %>% 
+                set_colnames(c('id', 'name', 'Hit_Points', 'Attack',
+                               'Defense', 'Special_Attack',
+                               'Special_Defense', 'Speed', 'Total'))
 
 
-# class属性がtdタグ
-title_nodes <- html_nodes(pokemon_ranking, "td")
-
-length(title_nodes)
-
-text <- html_text(title_nodes)
-print(text)
-
-
-pokemon_data <- matrix(text,ncol = 9,byrow = TRUE)
 
 
 
